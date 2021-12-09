2682c385d862be33a773004353a2e2f01a5fddc5
JGabrielTobares <gabriel.tobares@jala-foundation.org>
2021-12-09 16:54:28 -0300

    First commit

---
+++ test/SecondVController.swift
@@ -0,0 +1,22 @@
+//
+//  SecondVController.swift
+//  test
+//
+//  Created by Julio Gabriel Tobares on 07/12/2021.
+//
+
+import UIKit
+
+class SecondVController: UIViewController {
+
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
